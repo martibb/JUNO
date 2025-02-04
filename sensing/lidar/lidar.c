550bafb2e7dd1a18f5af3b39376d0da7c8491cb3
Martina <martina.burgisi@gmail.com>
2025-02-04 13:10:48 +0100

    Usage of leds on lidar

--- sensing/lidar/lidar.c
+++ sensing/lidar/lidar.c
@@ -6,6 +6,8 @@
 #include "stdlib.h"
 #include "stdbool.h"
 #include "./lidar.h"
+#include "os/dev/button-hal.h"
+#include "os/dev/leds.h"
 
 #define LOG_MODULE "LiDAR"
 #define LOG_LEVEL LOG_LEVEL_DBG
@@ -20,6 +22,7 @@ process_event_t LIDAR_STOP_EVENT;
 #define LIDAR_SAMPLING_INTERVAL 5   // Measurement interval (seconds)
 
 int publishing_enabled = 1; // 1 = Publish, 0 = Pause publication
+bool test_running = 0; // 0 = explore session, 1 = test session
 
 typedef struct {
     int distance_front;
@@ -31,11 +34,28 @@ int generate_lidar_value() {
     return ((float)rand() / (float)RAND_MAX) < LIDAR_OBSTACLE_PROBABILITY ? (rand() % LIDAR_MAX_DISTANCE) : LIDAR_MAX_DISTANCE;
 }
 
+void update_leds(int distance) {
+    if (distance== 100) {
+        leds_on(LEDS_GREEN);
+        leds_off(LEDS_YELLOW);
+        leds_off(LEDS_RED);
+    } else if (distance < 100 && distance > 20) {
+        leds_off(LEDS_GREEN);
+        leds_on(LEDS_YELLOW);
+        leds_off(LEDS_RED);
+    } else if (distance <= 20) {
+        leds_off(LEDS_GREEN);
+        leds_off(LEDS_YELLOW);
+        leds_on(LEDS_RED);
+    }
+}
+
 PROCESS(lidar_sensor_process, "LiDAR sensor process");
 
 PROCESS_THREAD(lidar_sensor_process, ev, data) {
     static struct etimer et;
     static struct process *subscriber;
+    button_hal_button_t *btn;
     static lidar_data_t lidar_data;
 
     PROCESS_BEGIN();
@@ -48,6 +68,8 @@ PROCESS_THREAD(lidar_sensor_process, ev, data) {
     LIDAR_SUB_EVENT = process_alloc_event();
     LIDAR_ALARM_EVENT = process_alloc_event();
     LIDAR_STOP_EVENT = process_alloc_event();
+
+    btn = button_hal_get_by_index(0);
 
     while(true) {
         PROCESS_WAIT_EVENT_UNTIL(ev == LIDAR_SUB_EVENT);
@@ -72,6 +94,8 @@ PROCESS_THREAD(lidar_sensor_process, ev, data) {
 
             if (ev == PROCESS_EVENT_TIMER && etimer_expired(&et) && publishing_enabled) {
                 lidar_data.distance_front = generate_lidar_value();
+                update_leds(lidar_data.distance_front);
+
                 lidar_data.distance_right = generate_lidar_value();
                 lidar_data.distance_left = generate_lidar_value();
 
@@ -80,6 +104,12 @@ PROCESS_THREAD(lidar_sensor_process, ev, data) {
 
                 process_post(subscriber, LIDAR_DISTANCE_EVENT, &lidar_data);
                 etimer_reset(&et);
+            }
+
+            if  (ev == button_hal_press_event) {
+                btn = (button_hal_button_t *)data;
+
+                printf("Press event (%s)\n", BUTTON_HAL_GET_DESCRIPTION(btn));
             }
         }
     }
